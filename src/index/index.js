ba5ddb0344b2c877b07d97b2955c9d15d0226e5e
wenhai wu <1725293885@qq.com>
2018-01-25 09:32:02 +0800

    init

--- src/index/index.js
+++ src/index/index.js
@@ -3,7 +3,7 @@ require('./index.scss');
 require('../asset/bootstrap/css/bootstrap.min.css');
 
 $(document).ready(function(){
-  let userInfo = localStorage.getItem("JianDiUserInfo");
+  let userInfo = localStorage.getItem("basicUserInfo");
   if (!userInfo) {
     location.href = '/login.html';
   }
